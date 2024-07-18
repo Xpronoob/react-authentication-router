5896aede42297e8e245ee550b14a24fa61479a6f
Xpronoob <kevin_03_puris@hotmail.com>
2024-07-18 10:06:23 -0600

    Apply DarkMode

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,37 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import { Logout } from './components/Logout'
 import { Dashboard } from './pages/Private/Dashboard'
+import Button from './components/ui/Button'
+
+import { useDarkMode } from './hooks/useDarkMode'
 
 const Login = lazy(() => import('./pages/Login/Login'))
 const Private = lazy(() => import('./pages/Private/Private'))
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode()
+
   return (
-  <div className="App">
-    <Suspense fallback={<>Cargando</>}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Logout />
-          <RoutesWithNotFound>
-            <Route path='/' element={<Navigate to ={PrivateRoutes.PRIVATE} />} />
-            <Route path={PublicRoutes.LOGIN} element={<Login />} />
-            <Route element={<AuthGuard privateValidation={true} />}>
-              <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<Private />} />
-            </Route>
-            <Route element={<RoleGuard rol={Roles.ADMIN} />}>
-              <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
-            </Route>
-          </RoutesWithNotFound>
-        </BrowserRouter>
-      </Provider>
-    </Suspense>
-  </div>
+    <div className="App bg-white dark:bg-slate-900 min-h-screen text-gray-200">
+      <Suspense fallback={<>Cargando</>}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Button onClick={toggleTheme}>🌒🌘{theme}</Button>
+            <Logout />
+            <RoutesWithNotFound>
+              <Route path="/" element={<Navigate to={PrivateRoutes.PRIVATE} />} />
+              <Route path={PublicRoutes.LOGIN} element={<Login />} />
+              <Route element={<AuthGuard privateValidation={true} />}>
+                <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<Private />} />
+              </Route>
+              <Route element={<RoleGuard rol={Roles.ADMIN} />}>
+                <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
+              </Route>
+            </RoutesWithNotFound>
+          </BrowserRouter>
+        </Provider>
+      </Suspense>
+    </div>
   )
 }
 
