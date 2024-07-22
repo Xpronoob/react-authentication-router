206f66b3fdc30df0dac8cb4c34e2af779a1d7778
Xpronoob <kevin_03_puris@hotmail.com>
2024-07-21 20:23:00 -0600

    Login Page

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,14 +1,19 @@
+import React from 'react'
+
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-function Input(props: Props) {
+const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
     <input
+      ref={ref}
       className="block w-full rounded-md border-0 pl-1.5 py-1.5 text-gray-900 shadow-sm
-    ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
-    focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+      ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
+      focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
       {...props}
     />
   )
-}
+})
+
+Input.displayName = 'Input'
 
 export default Input
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,50 +1,56 @@
-// import { useDispatch } from 'react-redux'
-// import { getMorty } from '../../services'
-// import { createUser, resetUser, UserKey } from '../../redux/states/user'
-// import { useNavigate } from 'react-router-dom'
-// import { PrivateRoutes, PublicRoutes } from '../../models'
-// import { useEffect } from 'react'
-// import { clearLocalStorage } from '../../utilities'
+// Login.tsx
+import { createUser, resetUser, UserKey } from '../../redux/states/user'
+import { useNavigate } from 'react-router-dom'
+import { PrivateRoutes, PublicRoutes } from '../../models'
+import { useEffect, useState } from 'react'
+import { clearLocalStorage } from '../../utilities'
+import { useDispatch } from 'react-redux'
+import { postLogin } from '../../services'
 import { useForm } from 'react-hook-form'
+import Input from '../../components/ui/Input'
 
-function Login() {
-  // const dispatch = useDispatch()
-  // const navigate = useNavigate()
-
+const Login = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm()
 
-  console.log('errors')
-  console.log(errors.email?.message)
+  const [postError, setPostError] = useState<any>('')
 
-  // useEffect(() => {
-  //   clearLocalStorage(UserKey)
-  //   dispatch(resetUser())
-  //   navigate(`/${PublicRoutes.LOGIN}`, {replace: true})
-  // }, [])
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await postLogin(data.email, data.password)
 
-  // const login = async () => {
-  //   try {
-  //     const result = await getMorty()
-  //     // dispatch(createUser({...result, rol: Roles.USER}))
-  //     dispatch(createUser(result))
-  //     navigate(`/${PrivateRoutes.PRIVATE}`, {replace: true})
-  //   } catch (error) {}
-  // }
+      if (response.status === 200) {
+        dispatch(createUser(response.data.user))
+        navigate(`/${PrivateRoutes.PRIVATE}`, { replace: true })
+      } else {
+        // console.error('Login failed:', response.data)
+        // Todo: manage errors
+      }
+    } catch (error) {
+      setPostError(error)
+      // console.error('Error:', error)
+      // Todo: manage connection errors
+    }
+  }
+
+  useEffect(() => {
+    // Limpiar cualquier estado de usuario existente y localStorage al cargar el componente
+    dispatch(resetUser())
+    clearLocalStorage(UserKey)
+  }, [dispatch])
 
   return (
-    <div>
-      {/* <h2>LOGIN!</h2>
-      <button onClick={login}>LOGIN</button> */}
-      <form
-        onSubmit={handleSubmit(data => {
-          console.log(data)
-        })}
-      >
-        <input
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img src="" alt="" />
+      <h2 className="">Welcome again!</h2>
+      <p>Please login to continue</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
           type="text"
           placeholder="Email"
           {...register('email', {
@@ -53,12 +59,37 @@ function Login() {
               value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
               message: 'Invalid email address',
             },
+            maxLength: {
+              value: 50,
+              message: 'Email must be less than 50 characters',
+            },
           })}
         />
-        {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email?.message}</p>}
+        {errors.email && <p className="mt-2 text-md text-red-600">{errors.email?.message}</p>}
+
+        <Input
+          type="password"
+          placeholder="Password"
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters',
+            },
+          })}
+        />
+        {errors.password && (
+          <p className="mt-2 text-md text-red-600">{errors.password?.message}</p>
+        )}
+
+        {postError.response?.status == 400 && (
+          <p className="mt-2 text-md text-red-600">User not found</p>
+        )}
+
         <button type="submit">LOGIN</button>
       </form>
     </div>
   )
 }
+
 export default Login
