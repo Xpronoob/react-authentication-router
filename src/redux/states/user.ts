b7d8ce56666f1e72d68484a658a78a99981cee62
Xpronoob <kevin_03_puris@hotmail.com>
2024-07-24 20:45:28 -0600

    User Roles fixed in redux state

--- src/redux/states/user.ts
+++ src/redux/states/user.ts
@@ -1,35 +1,38 @@
-import { createSlice, Slice } from "@reduxjs/toolkit"
-import { Roles, UserInfo } from "../../models"
-import { clearLocalStorage, persistLocalStorage } from "../../utilities"
+import { createSlice, Slice } from '@reduxjs/toolkit'
+import { UserInfo } from '../../models'
+import { clearLocalStorage, persistLocalStorage } from '../../utilities'
 
-export const EmptyUserState: UserInfo ={
+export const EmptyUserState: UserInfo = {
   id: 0,
   name: '',
   email: '',
-  rol: Roles.USER
+  roles: [],
 }
 
 export const UserKey = 'user'
 
 export const userSlice: Slice<UserInfo> = createSlice({
   name: 'user',
-  initialState: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') as string): EmptyUserState,
+  initialState: localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user') as string)
+    : EmptyUserState,
   reducers: {
-    createUser:(state, action) => {
+    createUser: (state, action) => {
       persistLocalStorage<UserInfo>(UserKey, action.payload)
       return action.payload
     },
 
     updateUser: (state, action) => {
-      const result = {...state, ...action.payload}
+      const result = { ...state, ...action.payload }
       persistLocalStorage<UserInfo>(UserKey, result)
       return result
     },
 
     resetUser: () => {
       clearLocalStorage(UserKey)
-      return EmptyUserState}
-  }
+      return EmptyUserState
+    },
+  },
 })
 
 export const { createUser, updateUser, resetUser } = userSlice.actions
