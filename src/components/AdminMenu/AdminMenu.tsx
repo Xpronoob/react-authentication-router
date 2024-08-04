1d6320b8be3bf856ed70d1c94f06f9e519d3c3e7
Xpronoob <kevin_03_puris@hotmail.com>
2024-08-04 15:16:12 -0600

    Admin Menu minor fixes and admin menu mock updated

--- src/components/AdminMenu/AdminMenu.tsx
+++ src/components/AdminMenu/AdminMenu.tsx
@@ -1,22 +1,36 @@
 import { Link } from 'react-router-dom'
-import { adminMenuData, AdminMenuItem, ListItem } from '../../mocks/adminMenuData'
+import { adminMenuData } from '../../mocks/adminMenuData'
 
 function AdminMenu() {
   return (
     <div className="flex flex-col bg-slate-100 h-screen">
-      {adminMenuData.map((item: AdminMenuItem) => (
-        <div className="" key={item.id}>
-          <span className="font-bold pl-4">{item.title}</span>
-          {item.listItems.map((listItem: ListItem) => (
-            <Link to={listItem.url} key={listItem.id} className="flex pl-2 gap-1">
-              <img src={listItem.icon} alt={`${listItem.title} icon`} />
-              <span>{listItem.title}</span>
+      {adminMenuData.map(subtitle => (
+        <div className="" key={subtitle.id}>
+          <span className="font-bold pl-4">{subtitle.title}</span>
+          {subtitle.listItems.map(item => (
+            <Link to={item.url} key={item.id} className="flex pl-2 gap-1">
+              <img src={item.icon} alt={`${item.title} icon`} />
+              <span>{item.title}</span>
             </Link>
           ))}
         </div>
       ))}
     </div>
   )
+  // return (
+  //   <div>
+  //     {adminMenuData.map(subtitle => (
+  //       <div key={subtitle.id}>
+  //         <span>{subtitle.title}</span>
+  //         {subtitle.listItems.map(item => (
+  //           <Link to={item.url} key={item.id}>
+  //             <span>{item.title}</span>
+  //           </Link>
+  //         ))}
+  //       </div>
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default AdminMenu
--- src/mocks/adminMenuData.ts
+++ src/mocks/adminMenuData.ts
@@ -1,3 +1,4 @@
+import { AdminRoutes } from '../models/routes'
 export interface ListItem {
   id: number
   title: string
@@ -14,36 +15,72 @@ export interface AdminMenuItem {
 export const adminMenuData: AdminMenuItem[] = [
   {
     id: 1,
-    title: 'Title',
+    title: 'Main',
     listItems: [
       {
         id: 1,
-        title: 'Link',
-        url: '/',
-        icon: '/vite.svg',
-      },
-      {
-        id: 2,
-        title: 'Link',
-        url: '/',
+        title: 'Homepage',
+        url: `/${AdminRoutes.ADMIN}`,
         icon: '/vite.svg',
       },
     ],
   },
   {
     id: 2,
-    title: 'Title',
+    title: 'Manage',
     listItems: [
       {
         id: 1,
-        title: 'Link',
-        url: '/',
+        title: 'Users',
+        url: `/${AdminRoutes.ADMIN}/users`,
         icon: '/vite.svg',
       },
       {
         id: 2,
-        title: 'Link',
-        url: '/',
+        title: 'Categories',
+        url: `/${AdminRoutes.ADMIN}/categories`,
+        icon: '/vite.svg',
+      },
+      {
+        id: 3,
+        title: 'Products',
+        url: `/${AdminRoutes.ADMIN}/products`,
+        icon: '/vite.svg',
+      },
+      {
+        id: 4,
+        title: 'Orders',
+        url: `/${AdminRoutes.ADMIN}/orders`,
+        icon: '/vite.svg',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Others',
+    listItems: [
+      {
+        id: 1,
+        title: 'Location',
+        url: `/${AdminRoutes.ADMIN}/location`,
+        icon: '/vite.svg',
+      },
+      {
+        id: 2,
+        title: 'Settings',
+        url: `/${AdminRoutes.ADMIN}/settings`,
+        icon: '/vite.svg',
+      },
+      {
+        id: 3,
+        title: 'Pages',
+        url: `/${AdminRoutes.ADMIN}/pages`,
+        icon: '/vite.svg',
+      },
+      {
+        id: 4,
+        title: 'Support',
+        url: `/${AdminRoutes.ADMIN}/support`,
         icon: '/vite.svg',
       },
     ],
