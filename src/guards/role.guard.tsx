ce70a8ac3d34e3e35e54c89b54240bad34450791
Xpronoob <kevin_03_puris@hotmail.com>
2024-09-21 20:19:35 -0600

    Role guard fix

--- src/guards/role.guard.tsx
+++ src/guards/role.guard.tsx
@@ -4,13 +4,13 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { AppStore } from '../redux/store'
 
 interface Props {
-  role: Roles
+  role?: Roles
+  roles: string[]
 }
-
-function RoleGuard({ role }: Props) {
+function RoleGuard({ roles }: Props) {
   const userState = useSelector((store: AppStore) => store.user)
   if (!userState.roles) return <Navigate replace to={PrivateRoutes.PRIVATE} />
-  return userState.roles.includes(role) ? (
+  return userState.roles.some(role => roles.includes(role)) ? (
     <Outlet />
   ) : (
     <Navigate replace to={PrivateRoutes.PRIVATE} />
